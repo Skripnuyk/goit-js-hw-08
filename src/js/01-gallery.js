c359cf146936f4a7c0d35de5b40f7aabbb26536e
Skripnuyk <walker8613@gmail.com>
2023-02-25 17:05:11 +0200

    add task 1/task 2 solution

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,28 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(element => {
+    const galleryLink = document.createElement('a');
+    galleryLink.classname = 'gallery__item';
+    galleryLink.href = element.original;
+    const galleryImage = document.createElement('img');
+    galleryImage.className = 'gallery__image';
+    galleryImage.src = element.preview;
+    galleryImage.alt = element.description;
+    galleryImage.setAttribute('title', element.description);
+
+    galleryLink.append(galleryImage);
+    items.push(galleryLink);
+});
+
+gallery.append(...items);
+
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
