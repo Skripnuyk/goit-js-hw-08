c9684b91d060d07801ded3b69df4ab34091d3b49
Skripnuyk <walker8613@gmail.com>
2023-02-26 20:09:49 +0200

    Fixed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,14 +3,14 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
+const CURRENT_TIME = 'videoplayer-current-time';
 player.on('timeupdate',  throttle( e => {
-    localStorage.setItem('videoplayer-current-time', e.seconds);
+    localStorage.setItem(CURRENT_TIME, e.seconds);
     }, 1000)
     );
 
 player
-.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0)
 .catch(function (error) {
     console.error(error)
 });
